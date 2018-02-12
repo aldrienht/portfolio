52234f4fba27069bc969c0a49aa88ef55e4ebab8
Aldrien Hate <aldrienhate@yahoo.com>
2018-02-12 10:51:57 +0800

    Updated a to Navlink in profile pic

--- src/components/navigation_links.jsx
+++ src/components/navigation_links.jsx
@@ -9,12 +9,12 @@ class NavigationLinks extends React.Component{
 
 		return(
 	    <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-	      <a className="navbar-brand" href="/about">
+	      <NavLink className="navbar-brand" to="/">
 	        <span className="d-block d-lg-none">Aldrien Hate</span>
 	        <span className="d-none d-lg-block">
 	          <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={Aldrien} alt="" />
 	        </span>
-	      </a>
+	      </NavLink>
 	      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	        <span className="navbar-toggler-icon"></span>
 	      </button>
